4c15c07a4695973ede5059f39d2fb312e5c358bc
Daria Bokareva <52369969+dariabokareva@users.noreply.github.com>
2021-09-05 15:04:18 +0300

    Create exapmle_v2.py
    autotest

---
+++ classic/aco_v2/exapmle_v2.py
@@ -0,0 +1,33 @@
+import numpy as np
+from scipy import spatial
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+num_points = 25
+
+points_coordinate = np.random.rand(num_points, 2)  # generate coordinate of points
+distance_matrix = spatial.distance.cdist(points_coordinate, points_coordinate, metric='euclidean')
+
+
+def cal_total_distance(routine):
+    num_points, = routine.shape
+    return sum([distance_matrix[routine[i % num_points], routine[(i + 1) % num_points]] for i in range(num_points)])
+
+
+# %% Do ACA
+from ant_colony_v2 import Ant_Colony
+
+aca = Ant_Colony(func=cal_total_distance, n_dim=num_points,
+                 size_pop=50, max_iter=200,
+                 distance_matrix=distance_matrix)
+
+best_x, best_y = aca.run()
+
+# %% Plot
+fig, ax = plt.subplots(1, 2)
+best_points_ = np.concatenate([best_x, [best_x[0]]])
+best_points_coordinate = points_coordinate[best_points_, :]
+ax[0].plot(best_points_coordinate[:, 0], best_points_coordinate[:, 1], 'o-r')
+pd.DataFrame(aca.y_best_history).cummin().plot(ax=ax[1])
+plt.show()
